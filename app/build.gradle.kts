a33474999a3781e1ace89f6df3026a010c60fcd3
tanimul <tanimulislam.fuad@gmail.com>
2025-02-12 10:28:28 +0600

    Converted Gradle Migration kts.

---
+++ app/build.gradle.kts
@@ -0,0 +1,109 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+
+    alias(libs.plugins.google.dagger.hilt.android)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.google.devtools.ksp)
+    alias(libs.plugins.navSafeArgs)
+    alias(libs.plugins.kotlin.parcelize)
+}
+
+android {
+    namespace = "com.tanimul.android_template_kotlin"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.tanimul.android_template_kotlin"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+        buildConfig = true
+    }
+
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // Lifecycle components
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.common.java8)
+
+    // Coroutines
+    api(libs.coroutines.core)
+    api(libs.coroutines.android)
+    implementation(libs.coroutines.core)
+
+    //Shimmer Effect
+    implementation(libs.shimmer)
+
+    // Circular Imageview
+    implementation(libs.circleimageview)
+
+    // Glide
+    implementation(libs.glide.library)
+    annotationProcessor(libs.glide.compiler)
+
+    //Lottie animation
+    implementation(libs.lottie)
+
+    // Retrofit
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.converter.gson)
+    implementation(libs.okhttp.logging.interceptor)
+
+    // Gson
+    implementation(libs.gson)
+
+    //Room for local data storage
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+    androidTestImplementation(libs.androidx.room.testing)
+
+    // Logging with Timber
+    implementation(libs.timber)
+
+    // Dependency Injection with Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+    implementation(libs.hilt.navigation.fragment)
+
+    // Data storage and preferences
+    implementation(libs.androidx.datastore.preferences)
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        maven { url = uri("https://jitpack.io") }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        maven { url = uri("https://jitpack.io") }
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Android-Template-Kotlin"
+include(":app")
